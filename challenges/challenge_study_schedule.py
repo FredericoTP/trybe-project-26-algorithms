557036709ec2aaf441c5b79d88a2948d63c6e832
Frederico Tavares <fredericotp@hotmail.com>
2023-06-01 14:16:45 -0300

    Feat: Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,23 @@
+def verify(element):
+    if element is None:
+        return True
+    if isinstance(element, str):
+        return True
+
+    return False
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for student in permanence_period:
+        if verify(student[0]) or verify(student[1]):
+            return None
+
+        if target_time >= student[0] and target_time <= student[1]:
+            count += 1
+
+    return count
