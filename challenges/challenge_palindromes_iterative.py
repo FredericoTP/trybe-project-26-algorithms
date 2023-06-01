dacabfa5ebbb9e480057e23e2d03afce57f425cd
Frederico Tavares <fredericotp@hotmail.com>
2023-06-01 15:26:24 -0300

    Feat: Requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,21 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    is_palindrome = False
+    low_index = 0
+    high_index = len(word) - 1
+
+    while high_index >= low_index:
+        if word[low_index] == word[high_index]:
+            is_palindrome = True
+        else:
+            is_palindrome = False
+
+        low_index += 1
+        high_index -= 1
+
+    return is_palindrome
