779b6daa5f3f779e6f1f546adcce20f2967a29b3
Frederico Tavares <fredericotp@hotmail.com>
2023-06-01 17:13:56 -0300

    Feat: Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(3, 3)
+
+    with pytest.raises(TypeError):
+        encrypt_message("banana", "abc")
+
+    assert encrypt_message("banana", 3) == "nab_ana"
+    assert encrypt_message("banana", 4) == "an_anab"
+    assert encrypt_message("banana", 15) == "ananab"
